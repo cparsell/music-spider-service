64f5c25a4fa21c9e8008ec774aa47a61c405da2b
cparsell <{ID}+{username}@users.noreply.github.com>
2026-07-11 22:00:18 -0700

    Update menu style

--- app/components/HomeClient.js
+++ app/components/HomeClient.js
@@ -46,7 +46,7 @@ export default function HomeClient({ defaultTab, isConfigured }) {
               onClick={() => setTab(t.id)}
               className={`px-3 py-2 -mb-0.5 border-b-2 lg:mb-0 lg:border-b-0 lg:border-l-2 lg:pl-6 lg:text-left  ${
                 tab === t.id
-                  ? "border-neutral-900 font-semibold bg-neutral-200 text-neutral-900"
+                  ? "border-neutral-900 bg-neutral-200 text-neutral-900"
                   : "border-transparent text-neutral-500 cursor-pointer "
               }`}
             >
